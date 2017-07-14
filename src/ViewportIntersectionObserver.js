278c551d4a64a912f1007868f479a53c91eeea4b
bonar <bonar@me.com>
2017-07-14 12:47:46 +0900

    add viewport bottom calcuration and test

--- src/ViewportIntersectionObserver.js
+++ src/ViewportIntersectionObserver.js
@@ -2,5 +2,31 @@
 
 export default class ViewportIntersectionObserver {
 
+  constructor () {
+    this.registry = [];
+  }
+
+  addListener(element, handlers) {
+  }
+
+  observe() {
+
+  }
+
+  getViewpointBottom() {
+    var win = this._getWindow();
+    var doc = this._getDocument();
+    return win.innerHeight 
+      + (doc.documentElement.scrollTop || win.pageYOffset);
+  }
+
+  _getWindow() {
+    return window;
+  }
+
+  _getDocument() {
+    return document;
+  }
+
 }
 
--- test/ViewportIntersectionObserver-test.js
+++ test/ViewportIntersectionObserver-test.js
@@ -4,9 +4,50 @@ import ViewportIntersectionObserver from "../src/ViewportIntersectionObserver.js
 
 describe("ViewportIntersectionObserver", () => {
 
+  let stubDOMFunctionality = (injectee) => {
+    injectee._getWindow = () => {
+      return {
+        innerHeight: 900
+      }
+    }
+    injectee._getDocument = () => {
+      return {
+        documentElement: {
+          scrollTop: 100
+        }
+      }
+    }
+  }
+
   it("initialize", () => {
     var VObserver = new ViewportIntersectionObserver();
     assert.ok(VObserver);
+  });
+
+  describe("viewpoint bottom", () => {
+
+    it("scrollTop", () => {
+      var VObserver = new ViewportIntersectionObserver();
+      stubDOMFunctionality(VObserver);
+      assert.equal(VObserver.getViewpointBottom(), 1000);
+    });
+
+    it("pageYOffset", () => {
+      var VObserver = new ViewportIntersectionObserver();
+      VObserver._getDocument = () => {
+        return {
+          documentElement: {}
+        }
+      };
+      VObserver._getWindow = () => {
+        return {
+          innerHeight: 900,
+          pageYOffset: 50
+        }
+      };
+      assert.equal(VObserver.getViewpointBottom(), 950);
+    });
+
   });
 
 });
